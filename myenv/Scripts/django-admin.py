20bcfbbfc902125e18f0cf539fbcdefd3c744366
volansinfo <124237447+volansinfo@users.noreply.github.com>
2024-04-11 13:07:10 +0530

    code update to main branch

--- myenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\lenovo1\desktop\127\riskcloud-master\backend\myenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
